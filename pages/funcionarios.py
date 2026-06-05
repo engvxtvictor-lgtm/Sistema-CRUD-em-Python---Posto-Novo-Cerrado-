dd8884ae4fdf09fd6ecff8e204d32585368d5408
Jose Victor de Moura Rufino <engvxtvictor@gmail.com>
2026-06-05 16:45:24 -0300

    adicionei a função de cadastrar funcionário

--- pages/funcionarios.py
+++ pages/funcionarios.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from crud import cadastrar_funcionario
 
 st.title("👨‍💼 Gerenciamento de Funcionários")
 
@@ -13,7 +14,48 @@ aba_cadastro, aba_listagem, aba_busca, aba_atualizacao, aba_exclusao = st.tabs(
 )
 
 with aba_cadastro:
+
     st.subheader("Cadastrar Funcionário")
+
+    with st.form("form_funcionario"):
+
+        nome = st.text_input("Nome")
+
+        cpf = st.text_input("CPF")
+
+        telefone = st.text_input("Telefone")
+
+        cargo = st.selectbox(
+            "Cargo",
+            [
+                "Frentista",
+                "Frentista Gerente",
+                "Dono do Posto"
+            ]
+        )
+
+        data_admissao = st.date_input(
+            "Data de Admissão"
+        )
+
+        cadastrar = st.form_submit_button(
+            "Cadastrar Funcionário"
+        )
+
+        if cadastrar:
+
+            sucesso, mensagem = cadastrar_funcionario(
+                nome,
+                cpf,
+                telefone,
+                cargo,
+                str(data_admissao)
+            )
+
+            if sucesso:
+                st.success(mensagem)
+            else:
+                st.error(mensagem)
 
 with aba_listagem:
     st.subheader("Listar Funcionários")
