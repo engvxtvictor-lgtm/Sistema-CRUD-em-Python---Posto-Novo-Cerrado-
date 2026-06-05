168c4a780064ed999f3240f679b037220806d822
Jose Victor de Moura Rufino <engvxtvictor@gmail.com>
2026-06-05 10:06:51 -0300

    implementar validações e geradores

---
+++ crud.py
@@ -0,0 +1,38 @@
+from dados import clientes, cpfs_cadastrados
+from utils import (
+    gerar_id,
+    validar_cpf,
+    validar_telefone,
+    validar_vazio,
+    cpf_ja_existe
+)
+
+
+def cadastrar_cliente(nome, cpf, telefone):
+    """
+    Cadastra um novo cliente no sistema.
+    """
+
+    if not validar_vazio(nome):
+        return False, "O nome não pode estar vazio."
+
+    if not validar_cpf(cpf):
+        return False, "CPF inválido. Deve conter exatamente 11 dígitos."
+
+    if cpf_ja_existe(cpf):
+        return False, "CPF já cadastrado."
+
+    if not validar_telefone(telefone):
+        return False, "Telefone inválido. Deve conter DDD + 9 dígitos."
+
+    novo_cliente = {
+        "id": gerar_id(clientes),
+        "nome": nome.strip(),
+        "cpf": cpf,
+        "telefone": telefone
+    }
+
+    clientes.append(novo_cliente)
+    cpfs_cadastrados.add(cpf)
+
+    return True, "Cliente cadastrado com sucesso!"
