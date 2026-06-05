3251dc3d42d0805d55f7785a73668d8b8ceade83
Jose Victor de Moura Rufino <engvxtvictor@gmail.com>
2026-06-05 10:08:19 -0300

    criação do app.py para rodar via streamlit (alternativa que eu achei pois n tava conseguindo rodar sem ele)

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Posto Novo Cerrado",
+    page_icon="⛽",
+    layout="wide"
+)
+
+st.title("⛽ Sistema de Gerenciamento do Posto Novo Cerrado")
+
+st.write("Selecione uma página no menu lateral.")
